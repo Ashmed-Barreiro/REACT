13dabedeca61279ec09d6725bece77782f856534
Ashmed-Barreiro <ashmed.barreiro.7e8@itb.cat>
2026-02-27 23:31:54 +0100

    fix Imagenes in user card

--- mi-app/src/pages/Users.jsx
+++ mi-app/src/pages/Users.jsx
@@ -1,34 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import "./users.css"
 
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const loadUsers = async () => {
-    try {
-      setLoading(true);
-      // Este fetch coge los datos de una web externa
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      const data = await res.json();
-      setUsers(data);
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  };
+  useEffect(() => {
+      const loadUsers = async () => {
+          // Este fetch coge los datos desde public en local
+        const res = await fetch("/data/db.json");
+        const data = await res.json();
+        setUsers(data);
+      };
+  
+      loadUsers();
+    }, []);
 
   return (
-    <div>
-      <h1>Users</h1>
-      <button onClick={loadUsers}>Cargar Usuarios</button>
-
-      <ul>
+    <div className="users-page">
+      <h1 className="users-title">Users</h1>
+      <ul className="users-list">
         {users.map((user) =>( 
-          <li key={user.id}>
-            Nombre: {user.name}<br></br>
-            Email: {user.email}<br></br>
+          <li className="user" key={user.id}>
+            <p>Nombre: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <img src={"/img/" + user.name+".png"} alt={ user.name + " Image"} />
             <NavLink to={"/details/" + user.id} >Detalles </NavLink>
           </li>
         ))}
